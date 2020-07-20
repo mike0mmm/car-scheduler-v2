04b54457db01f8941de1f88532634ff4b1bfebd1
Michael Valershtein <mike0mmm@gmail.com>
2020-07-20 22:27:02 +0300

    add db connection attempt

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
 
 	"github.com/gin-gonic/gin"
 	// "net/http"
@@ -12,9 +16,28 @@ func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "pong",
+			"message": handle(),
 		})
 	})
+
 	r.Run()
 
+}
+
+func handle() string {
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		return err.Error()
+	}
+	err = db.Ping()
+	if err != nil {
+		return err.Error()
+	}
+
+	_, err = db.Exec("CREATE TABLE [IF NOT EXISTS] mu_first_table (count INT NOT NULL);", nil)
+	if err != nil {
+		return err.Error()
+	} else {
+		return "OK"
+	}
 }
