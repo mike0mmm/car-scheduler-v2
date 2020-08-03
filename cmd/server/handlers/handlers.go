3bc2e19d151bc234c9ce41597b308069e7615ac6
Michael Valershtein <mike0mmm@gmail.com>
2020-08-03 18:30:17 +0300

    add error message

--- cmd/server/handlers/handlers.go
+++ cmd/server/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"database/sql"
+	"fmt"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -24,7 +25,8 @@ func (h *Handlers) GetPing() func(*gin.Context) {
 
 func (h *Handlers) GetInit() func(*gin.Context) {
 	return func(c *gin.Context) {
-		c.JSON(200, gin.H{
+        
+        c.JSON(200, gin.H{
 			"message": createTable(),
 		})
 	}
@@ -33,17 +35,17 @@ func (h *Handlers) GetInit() func(*gin.Context) {
 func createTable() string {
 	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
 	if err != nil {
-		return err.Error()
+		return fmt.Sprintf("failed to open connection. error: %s", err.Error()) 
 	}
 	err = db.Ping()
 	if err != nil {
-		return err.Error()
+		return fmt.Sprintf("failed to ping. error: %s", err.Error())
 	}
 
-	_, err = db.Exec("CREATE TABLE  my_first_table (count integer NOT NULL);", nil)
-	if err != nil {
-		return err.Error()
-	}
+	// _, err = db.Exec("CREATE TABLE  my_first_table (count integer NOT NULL);", nil)
+	// if err != nil {
+	// 	return err.Error()
+	// }
 
 	return "OK"
 }
